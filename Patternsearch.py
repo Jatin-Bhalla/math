6ce05051861ea2a5003ddeae06537e08fb987aea
PI <jatin241001@gmail.com>
2022-08-03 20:09:56 +0530

    Add files via upload

---
+++ Patternsearch.py
@@ -0,0 +1,13 @@
+from math import exp, log, sqrt
+import re
+
+#import the necessary modules
+# Count the number of times a pattern appears in a string
+string = input("Enter the sentence to search:")
+string_list = string.split()
+pattern = re.compile(r"The", re.I)
+count = 0
+for word in string_list:
+    if pattern.search(word):
+        count += 1
+print("Output #38: {0:d}".format(count))
